f0f81eca6933498160b191fc466826ffbabdbdee
MarchukSWA <135006609+MarchukSWA@users.noreply.github.com>
2024-04-30 14:27:02 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,67 +1,25 @@
-<?php
-$result = "";
-$first = 0;
-$second = 0;
-$function = "";
-    if (!empty($_POST['first'])):
-        $first = $_POST['first'];
-    endif;
-    if(!empty($_POST['second'])):
-        $second = $_POST['second'];
-    endif;        
-    $function = $_POST["function"];
-    switch($function):
-        case "+":
-            $result = $first + $second;
-            break;
-        case "-":
-            $result = $first - $second;
-            break;
-        case "*":
-            $result = $first * $second;
-            break;
-        case "/":
-            if ($second == 0):
-                $result = "N/A";
-            else:                
-                $result = $first / $second;
-            endif;                
-            break;
-        default:
-            break;
-        endswitch;
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <?php include 'calc.php';?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calculator</title>
-    <style>
-        body.dark-theme {
-            background-color: #333;
-            color: #fff;
-        }
-    </style>
-    <script>
-        function toggleDarkMode() {
-            document.body.classList.toggle("dark-theme");
-            localStorage.setItem("darkMode", document.body.classList.contains("dark-theme"));
-        }
-
-        function loadDarkModePreference() {
-            const darkModeEnabled = localStorage.getItem("darkMode") === "true";
-            if (darkModeEnabled) {
-                document.body.classList.add("dark-theme");
-            }
-        }
-
-        window.onload = loadDarkModePreference;
-    </script>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="buttons.css">
+    <link rel="stylesheet" href="button-bubbles.css">
+    <link rel="stylesheet" href="bar.css">
+    <script src="script.js"></script>
 </head>
 <body>
-    <form method="post" attribute="post" action="index.php">
+    <div class="topnav">
+        <a class="active" href="index.php">Home</a>
+        <a href="about.html">About</a>
+        <a href="constacts.html">Contact</a>
+      </div>
+      <div><button class="button" onclick="toggleDarkMode()">Toggle Dark Mode</button></div>
+      
+    <form method="post" attribute="post" action="calc.php">
         <p>First Value:<br/>
             <input type="text" id="first" name="first"></p>
 
@@ -75,11 +33,11 @@ $function = "";
         
         <p>Second Value:<br/>
             <input type="text" id="second" name="second"></p>
-        <button type="submit" name="answer" id="answer" value="answer">Calculate</button>
+        <button class="blob-btn" type="submit" name="answer" id="answer" value="answer">Calculate</button>
     </form>
+    <!-- <img src="images/gallery/<?php echo $result; ?>.png"> -->
 
     <p><?php echo $first. $function. $second. "=". $result; ?></p>
-    
-    <button onclick="toggleDarkMode()">Toggle Dark Mode</button>
+
 </body>
 </html>
